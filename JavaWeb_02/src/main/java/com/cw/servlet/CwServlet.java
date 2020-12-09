91e25ebdcde9401bd4bda635b3e3018f1f86a663
wc <1980700161@qq.com>
2020-12-09 21:02:24 +0800

    Initial commit

---
+++ JavaWeb_02/src/main/java/com/cw/servlet/CwServlet.java
@@ -0,0 +1,9 @@
+package com.cw.servlet;
+/*
+ *   @author 王超
+ *   @create 2020-12-09 15:56
+ *
+ */
+
+public class CwServlet {
+}
---
+++ JavaWeb_02/src/main/java/com/cw/servlet/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.cw.servlet;
+
+/*
+ *   @author 王超
+ *   @create 2020-12-09 9:55
+ *
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("username","java");
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
